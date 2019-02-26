8fbf42631c617bbdfdcc5c0d7b5509878bc3cdb8
joe-michel <joe.michel.f@gmail.com>
2019-02-26 16:57:54 +0100

    "DB change"

--- _pgsql-db/about.sql
+++ _pgsql-db/about.sql
@@ -4,6 +4,7 @@ DROP TABLE IF EXISTS "users";
 CREATE TABLE "public"."users" (
     "id" SERIAL PRIMARY KEY NOT NULL,
     "username" character varying(250) NOT NULL,
-    "password" character varying(250) NOT NULL
+    "password" character varying(250) NOT NULL,
+    "permissionId" int NOT NULL
 )
 -- 2019-02-21 11:11:52.20687+00
