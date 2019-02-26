415f0b9c060f3224da34f3c8348df3b8e6bada22
joe-michel <joe.michel.f@gmail.com>
2019-02-26 10:56:11 +0100

    Test "Users" database creation

--- _pgsql-db/about.sql
+++ _pgsql-db/about.sql
@@ -1,15 +1,9 @@
 -- Adminer 4.7.1 PostgreSQL dump
 
-DROP TABLE IF EXISTS "about";
-CREATE TABLE "public"."about" (
-    "id" integer NOT NULL,
-    "name" character varying(50) NOT NULL,
-    "profile" character varying(50) NOT NULL
-) WITH (oids = false);
-
-INSERT INTO "about" ("id", "name", "profile") VALUES
-(1,	'Jean',	'Developer'),
-(2,	'Pierre',	'Designer'),
-(3,	'Paul',	'Community manager');
-
+DROP TABLE IF EXISTS "users";
+CREATE TABLE "public"."users" (
+    "id" SERIAL PRIMARY KEY NOT NULL,
+    "username" character varying(250) NOT NULL,
+    "password" character varying(250) NOT NULL
+)
 -- 2019-02-21 11:11:52.20687+00
