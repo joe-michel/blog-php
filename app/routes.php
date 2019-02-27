d6ee007f3f0afad62c0a8728cced2a4c5134c1f4
joe-michel <joe.michel.f@gmail.com>
2019-02-27 11:31:02 +0100

    "Template modification"

--- app/routes.php
+++ app/routes.php
@@ -54,33 +54,3 @@ $app->post('/signup',function(ServerRequestInterface $request,ResponseInterface 
     'password' => $password));
   return $this->view->render($response, 'home.twig');
 })->setName('signup');
-
-// route for about +DB content
-$app->get('/about',function(ServerRequestInterface $request,ResponseInterface $response,$args) {
-  $sth = $this->db->prepare("SELECT id, name, profile FROM about");
-  $sth->execute();
-  $about = $sth->fetchAll();
-  //return $this->response->withJson($about);
-  return $this->view->render($response, 'about.twig', ['curl_result' => $about] );
-})->setName('about');
-
-// route for contact
-$app->get('/contact',function(ServerRequestInterface $request,ResponseInterface $response,$args) {
-  return $this->view->render($response, 'contact.twig');
-})->setName('contact');
-
-// Post Requests
-$app->post('/confirm',function(ServerRequestInterface $request,ResponseInterface $response,$args) {
-  //we put the content of the form in an array and then in a variable
-  $data = ['contentForm' => $request->getParam('contentForm')];
-  //because we can only send an array as argument in a render
-  return $this->view->render($response, 'confirm.twig', $data);
-})->setName('confirm');
-
-// get content of DB
-/*$app->get('/contendDb', function (ServerRequestInterface $request,ResponseInterface $response,$arg) {
-    $sth = $this->db->prepare("SELECT `id`, `name`, profile FROM `about`");
-    $sth->execute();
-    $about = $sth->fetchAll();
-    return $this->response->withJson($about);
-});*/
