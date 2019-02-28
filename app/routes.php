4ca1c903622bd896ef70b7d7781052972d72b938
joe-michel <joe.michel.f@gmail.com>
2019-02-28 09:31:39 +0100

    Différents templates for Users, Visitors, and Authors

--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,7 @@ use \Psr\Http\Message\ResponseInterface;
 // instance->http verb GET, POST, DELETE, PUT... ('URI', callBackFunction aka closure(PSR 7 request objec $HTTP request, PSR 7 request objec $HTTP response, $array passed to the URI))
 // route for HP
 $app->get('/',function(ServerRequestInterface $request,ResponseInterface $response,$args) {
-  return $this->view->render($response, 'home.twig');
+  return $this->view->render($response, 'Nav_Visitor.twig');
 })->setName('home');
 
 $app->post('/log', function(ServerRequestInterface $request,ResponseInterface $response, $args) {
@@ -26,16 +26,23 @@ $app->post('/log', function(ServerRequestInterface $request,ResponseInterface $r
   $isPasswordOk = password_verify($password, $fetch['password']);
   if (!$isPasswordOk) {
     echo "Le nom d'utilisateur ou le mot de passe est incorrect";
-    return $this->view->render($response, 'home.twig');
+    return $this->view->render($response, 'Nav_Visitor.twig');
   } else {
       session_start();
       $_SESSION['id'] = $fetch['id'];
       $_SESSION['permission'] = $fetch['permissionid'];
       $_SESSION['username'] = $username;
-      echo "Vous êtes un Beau Gosse";
-      return $this->view->render($response, 'home.twig', ['curl_result' => $_SESSION]);
+
+      if($_SESSION['permission'] === 0){
+        return $this->view->render($response, 'Nav_User.twig', ['curl_result' => $_SESSION]);
+      } else if ($_SESSION['permission'] === 1) {
+        return $this->view->render($response, 'Nav_Author.twig', ['curl_result' => $_SESSION]);
+      } else {
+        return $this->view->render($response, 'Nav_Admin.twig', ['curl_result' => $_SESSION]);
+      }
     }
-})->setName('log');
+
+})->setName('home');
 
 $app->get('/signup',function(ServerRequestInterface $request,ResponseInterface $response,$args) {
   return $this->view->render($response, 'signup.twig');
@@ -55,5 +62,5 @@ $app->post('/signup',function(ServerRequestInterface $request,ResponseInterface 
 $app->post('/disconnect',function(ServerRequestInterface $request,ResponseInterface $response,$args) {
   session_start();
   session_destroy();
-  return $this->view->render($response, 'home.twig');
-})->setName('disconnect');
+  return $this->view->render($response, 'Nav_Visitor.twig');
+})->setName('home');
