3f5d8a937de88f88e06e603108d339dbb39ec5b3
Louis <cantinieauxlouis@gmail.com>
2019-03-11 16:57:59 +0100

    Writing the deployment script in bash

--- deploy.sh
+++ deploy.sh
@@ -17,3 +17,17 @@ rsync --exclude-from=ExclusionRSync source/ destination/
 #                             .PlayOnLinux
 
 # rsync --progress option displays detailed progress of rsync execution as shown below.
+
+# To transfer a file securely to another machine - type:
+scp file1.txt example@server2:$HOME
+# This example presents transferring file1.txt from our host to server2 's user tom 's home directory.
+# scp can serve to send mutltiple files from a server to another, just send the all folder
+
+# Show all process paginated
+ps -e | less
+# ps -e shows all the processes, its output is connected to the input of more via | , less paginates the results.
+
+# the conditionnal staement (if) takes many flags to perform conditionnal tests
+# -e file is true if a specific file/directory exists
+# -z string is true if  the given string is empty
+# string1 = string2 is true if specific the two string are equals
